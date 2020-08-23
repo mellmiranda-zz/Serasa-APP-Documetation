6d6411d66c78c166c3a1ed15f745e0dc2c5b0e22
mellmiranda <melanie.miranda.ti@gmail.com>
2020-08-22 21:44:57 -0300

    add components

--- Documentation-Serasa/src/app/app.module.ts
+++ Documentation-Serasa/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
